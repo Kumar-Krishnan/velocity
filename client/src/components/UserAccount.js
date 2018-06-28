e45ef2376497c93f483517e0ae0eae326f1066cf
PsijicSeht <38445096+PsijicSeht@users.noreply.github.com>
2018-06-28 14:06:44 -0400

    user values showing in edit box

--- client/src/components/UserAccount.js
+++ client/src/components/UserAccount.js
@@ -9,13 +9,34 @@ class UserAccount extends Component {
         axios.get(`/database/users/${this.props.match.params.userId}`)
         .then((res)=>{
             console.log(res.data)
+            this.setState({user: res.data})
         })
     }
     render() {
         
         return (
             <div>
-                
+                <form onSubmit={this.handleSubmit}> 
+                    <div>
+                    <label>User Name:</label>
+                    <input
+                       type="text"
+                       name="name"
+                       value={this.state.user.name}
+                    //    onChange={this.handleChange}
+                    />
+
+                    </div>
+
+                    <div>
+                        <label>Password: </label>
+                        <input 
+                        value="PlaceHolder"
+                        name="password"
+                        value={this.state.user.password}/>
+                    </div>
+                    <button type="submit">Save Changes</button>
+                    </form>
             </div>
         );
     }
