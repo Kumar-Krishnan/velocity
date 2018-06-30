9f5b0b8ed82c58c64d14cde94e6d5376226b08f0
PsijicSeht <38445096+PsijicSeht@users.noreply.github.com>
2018-06-30 15:40:09 -0400

    further values styling

--- client/src/components/Value.js
+++ client/src/components/Value.js
@@ -6,6 +6,15 @@ const ValueBox = styled.div`
     background-color: #fffab1;
     border: 2px solid green;
     margin-bottom: 15px;
+    h6{
+        margin:10px;
+    }
+    .author{
+        margin-bottom: 5px;
+    }
+    button{
+        margin-bottom: 5px;
+    }
 `
 const RemoveValueButton = styled.button`
     background-color:transparent;
@@ -24,7 +33,7 @@ class Value extends Component {
                 <div>
                     {this.props.value.text}
                 </div>
-                <div>
+                <div className="author">
                     {this.props.value.author}
                 </div>
                 <RemoveValueButton onClick={this.handleValueRemove}>Remove Value</RemoveValueButton>
